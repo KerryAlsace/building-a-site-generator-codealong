9e8ef35caa37e0e730313eb34a1d48256e977395
KerryAlsace <kerrydougherty@gmail.com>
2016-08-30 15:02:46 -0400

    add individual pages generator

--- lib/site_generator.rb
+++ lib/site_generator.rb
@@ -19,11 +19,44 @@ class SiteGenerator
     File.write("#{path}/#{template_path}", html)
   end
 
+  def generate_artist_show
+    template_string = File.read("./lib/views/artists/show.html.erb")
+    template = ERB.new(template_string)
+
+    Artist.all.each do |artist|
+      html = template.result(binding)
+      File.write("#{path}/artists/#{artist.name}.html", html)
+    end
+  end
+
+  def generate_genre_show
+    template_string = File.read("./lib/views/genres/show.html.erb")
+    template = ERB.new(template_string)
+
+    Genre.all.each do |genre|
+      html = template.result(binding)
+      File.write("#{path}/genres/#{genre.name}.html", html)
+    end
+  end
+
+  def generate_song_show
+    template_string = File.read("./lib/views/songs/show.html.erb")
+    template = ERB.new(template_string)
+
+    Song.all.each do |song|
+      html = template.result(binding)
+      File.write("#{path}/songs/#{song.name}.html", html)
+    end
+  end
+
   def call
     generate_index("index.html")
     generate_index("artists/index.html")
     generate_index("genres/index.html")
     generate_index("songs/index.html")
+    generate_artist_show
+    generate_genre_show
+    generate_song_show
   end
 
 end
